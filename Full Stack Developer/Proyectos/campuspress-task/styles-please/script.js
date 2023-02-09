79820106dc39aa2249e4f0c47314b0a0869e73ca
RicardoJSanoja <107774830+RicardoJSanoja@users.noreply.github.com>
2023-02-08 20:07:38 -0400

    Add files via upload

---
+++ Full Stack Developer/Proyectos/campuspress-task/styles-please/script.js
@@ -0,0 +1,80 @@
+/*const item_main = document.querySelector('.main-div');
+
+const object_array = [];
+
+object_array.push({
+  name: "city foto",
+  imagen: "./walter-white.svg",
+  h1: "example text",
+  redirection: "https://github.com/RicardoJSanoja/Full-Stack-Developer-JS",
+});
+
+object_array.push({
+  name: "beach bay",
+  imagen: "./WordPress-logotype-simplified.png ",
+  h1: "example text",
+  redirection: "https://github.com/RicardoJSanoja/Full-Stack-Developer-JS",
+
+});
+
+object_array.push({
+  name: "forest",
+  imagen: "./WordPress-logotype-simplified.png",
+  h1: "example text",
+  redirection: "https://github.com/RicardoJSanoja/Full-Stack-Developer-JS",
+});
+
+object_array.push({
+  name: "field",
+  imagen: "./WordPress-logotype-simplified.png",
+  h1: "example text",
+  redirection: "https://github.com/RicardoJSanoja/Full-Stack-Developer-JS",
+});
+
+object_array.push({
+  name: "areal forest picture",
+  imagen: "./WordPress-logotype-simplified.png",
+  h1: "example text",
+  redirection: "https://github.com/RicardoJSanoja/Full-Stack-Developer-JS",
+});
+
+object_array.push({
+  name: "red house",
+  imagen: "./WordPress-logotype-simplified.png",
+  h1: "example text",
+  redirection: "https://github.com/RicardoJSanoja/Full-Stack-Developer-JS",
+});
+
+for(object in object_array){
+
+  //item_div
+  const item_div = document.createElement('div');
+  item_div.classList.add("item_div");
+  //img-h
+  const imgH = document.createElement('div');
+  imgH.classList.add("img-h");
+  //figure
+  const figureC = document.createElement('figure');
+  figureC.classList.add("figrueC");
+  // a href
+  const ref = document.createElement('a');
+  ref.setAttribute("href", object.redirection);
+  //img
+  const foto = document.createElement('img');
+  foto.setAttribute('src', object.imagen);
+  foto.setAttribute("alt", "img test");
+  foto.classList.add("item_img");
+  //h1
+  const h_text  = document.createElement('h1');
+  h_text.innerText = object.h1;
+  h_text.classList.add("h_text");
+
+  //union de los elementos
+  ref.appendChild(foto);
+  figureC.appendChild(ref);
+  imgH.appendChild(figureC);
+  imgH.appendChild(h_text);
+  item_div.appendChild(imgH);
+  item_main.appendChild(item_div);
+
+}*/
